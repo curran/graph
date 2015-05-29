158a9664055ca1819f97b20bed2f01e1680f227b
Curran <curran.kelleher@gmail.com>
2015-05-28 20:04:38 -0700

    Added initial implementation.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # graph
-A small graph implementation with depth first search.
+
+A graph implementation with depth first search.
+
+## Usage
+
+```javascript
+var graph = Graph();
+
+graph.addEdge("A", "B");
+graph.addEdge("B", "C");
+
+// prints ["B"]
+console.log(graph.adjacent("A"))
+
+// Perform Depth First Search (DFS)
+// nodes = ["C", "B", "A"]
+var nodes = graph.DFS(["A"]);
+
+var topologicallySorted = nodes.reverse();
+```
---
+++ graph.js
@@ -0,0 +1,43 @@
+export function Graph(){
+  
+  // Keys are node ids.
+  // Values are arrays of adjacent node ids.
+  var edges = {};
+  
+  function adjacent(u){
+    return edges[u] || (edges[u] = []);
+  }
+  
+  return {
+
+    addEdge: function (u, v){
+      adjacent(u).push(v);
+    },
+    removeEdge: function (u, v){ /* TODO */ },
+    removeNode: function (u){ /* TODO */ },
+
+    adjacent: adjacent,
+
+    DFS: function (originNodes){
+
+      // Keys are node ids.
+      // Values are true when the node has been visited.
+      var visited = {};
+      
+      // An array of nodes containing the result of DFS().
+      var nodes = [];
+
+      function DFSVisit(u){
+        if(!visited[u]){
+          visited[u] = true;
+          adjacent(u).forEach(DFSVisit);
+          nodes.push(u);
+        }
+      }
+  
+      originNodes.forEach(DFSVisit);
+
+      return nodes;
+    }
+  };
+}
